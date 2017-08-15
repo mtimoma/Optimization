abba06c4d1445ac7bf3728c0e754851e7282715e
mtimoma <mtimoma@gmail.com>
2017-08-15 14:17:17 -0700

    Bayes Test Update
    Messed something up. This is proof that order does not matter in calculating Ph

--- MemoryLeakTest.py
+++ MemoryLeakTest.py
@@ -1,21 +1,18 @@
 import numpy as np
 
 def neuron_resp(a):
-
+    np.random.seed(1000)
     return np.random.poisson(50*X[0][a])
 
 X = np.load('./data/all_resp.npy')
 Pr_h = np.load('./data/probh50.npy')
 
-
-np.random.seed(1000)
 Ph = [0.02]*41
 for i in range(0,5,1):
     response = neuron_resp(i)
     Ph = Pr_h[i,response,:]*Ph/sum(Pr_h[i,response,:]*Ph)
 print(Ph)
 
-np.random.seed(1000)
 Ph = [0.02]*41
 for i in range(4,-1,-1):
     response = neuron_resp(i)
